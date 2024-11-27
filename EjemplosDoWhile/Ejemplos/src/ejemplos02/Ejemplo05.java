3de6f89df1a05f0851de48a0c78d6365a519fb46
unknown <luiseduardosa2006@gmail.com>
2024-11-27 11:55:54 -0500

    s

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -7,6 +7,7 @@
  sean ingresados por teclado por el usuario.
  */
 package ejemplos02;
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -14,6 +15,8 @@ package ejemplos02;
 public class Ejemplo05 {
 
     public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
         // 2. numero, i[0, n]
         int numero;
         // 3. potencia, i[0, n]
@@ -21,9 +24,13 @@ public class Ejemplo05 {
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
+        
+        System.out.println("Ingrese el numero base");
+        numero = entrada.nextInt();
         // 8. potencia <-- 3
-        potencia = 3;
+        
+        System.out.println("Ingrese el numero potencia");
+        potencia = entrada.nextInt();
         // 9. resultado <-- 1
         resultado = 1;
 
